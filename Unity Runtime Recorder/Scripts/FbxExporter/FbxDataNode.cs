4663fcfc94434f67a0dd0c97bfb9bc8f1f317c92
newyellow <newyellow2@gmail.com>
2017-10-10 03:36:14 +0800

    Add FBX Export function

---
+++ Unity Runtime Recorder/Scripts/FbxExporter/FbxDataNode.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class FbxDataNode
+{
+	public static FbxDataNode[] FetchNodes (string inputData, int level)
+	{
+		List<FbxDataNode> nodes = new List<FbxDataNode> ();
+
+		StringReader reader = new StringReader (inputData);
+
+		bool foundBracket = false;
+
+		string[] tempNodeData = new string[2];
+
+		while (reader.Peek () != -1) {
+			
+			string strLine = reader.ReadLine ();
+
+			// found bracket, fetch subNodes
+			if (strLine.IndexOf ("{") != -1) {
+
+				int bracketNum = 1;
+				string[] nodeData = FbxDataNode.GetNodeData (strLine);
+				string contentInsideBracket = "";
+
+				// search for bracket end
+				while (reader.Peek () != -1) {
+					string insideBracketLine = reader.ReadLine ();
+
+					if (insideBracketLine.IndexOf ("{") != -1) {
+						contentInsideBracket += insideBracketLine + "\n";
+						++bracketNum;
+					} else if (insideBracketLine.IndexOf ("}") != -1) {
+						--bracketNum;
+
+						if (bracketNum == 0) {
+							break;
+						} else {
+							contentInsideBracket += insideBracketLine + "\n";
+						}
+					} else
+						contentInsideBracket += insideBracketLine + "\n";
+				}
+
+				FbxDataNode newNode = new FbxDataNode (nodeData [0], nodeData [1], level);
+				FbxDataNode[] subNodes = FbxDataNode.FetchNodes (contentInsideBracket, level + 1);
+
+				for (int i = 0; i < subNodes.Length; i++)
+					newNode.addSubNode (subNodes [i]);
+
+				nodes.Add (newNode);
+			}
+			// found attribute, add to a subnode
+			else if (strLine.IndexOf (":") != -1) {
+
+				string[] nodeData = FbxDataNode.GetNodeData (strLine);
+				FbxDataNode newNode = new FbxDataNode (nodeData [0], nodeData [1], level);
+
+				nodes.Add (newNode);
+			}
+			// found nothing, just skip
+			else {
+				;
+			}
+		}
+
+		return nodes.ToArray ();
+	}
+
+	public static string[] GetNodeData (string strLine)
+	{
+
+		string searchPattern = "";
+
+		if (strLine.IndexOf ("{") != -1) {
+			searchPattern = "([^:]*):([^\\{]*)\\{";
+		} else {
+			searchPattern = "([^:]*):([^\\n]*)\\n";
+			strLine += "\n";
+		}
+		Match matchData = Regex.Match (strLine, searchPattern);
+
+		string[] resultData = new string[2];
+		if (matchData.Success) {
+			resultData [0] = matchData.Groups [1].Value;
+			resultData [1] = matchData.Groups [2].Value;
+
+			// clear \t
+			resultData [0] = resultData [0].Replace ("\t", "");
+		} else {
+			Debug.Log ("ERROR :: Cant Get Node Data");
+		}
+
+		return resultData;
+	}
+
+
+	public string nodeName;
+	public string nodeData;
+
+	int level = 0;
+
+	public List<FbxDataNode> subNodes;
+	bool hasSubNode {
+		get{
+			if (subNodes.Count > 0)
+				return true;
+			else
+				return false;
+		}
+	}
+
+	// create new data
+	public FbxDataNode (string nodeName, string nodeData, int level)
+	{
+		subNodes = new List<FbxDataNode> ();
+		this.nodeName = nodeName;
+		this.nodeData = nodeData;
+		this.level = level;
+	}
+
+	public void addSubNode (FbxDataNode newNode)
+	{
+		subNodes.Add (newNode);
+	}
+
+	public string getResultData ()
+	{
+		string resultString = "";
+		string levelBlanks = "";
+
+		for (int i = 0; i < level; i++)
+			levelBlanks += "\t";
+
+		resultString += levelBlanks + nodeName + ": " + nodeData;
+
+		if (hasSubNode) {
+			resultString += " {\n";
+
+			for (int i = 0; i < subNodes.Count; i++) {
+				resultString += subNodes [i].getResultData ();
+			}
+
+			resultString += levelBlanks + "}\n";
+		} else {
+			resultString += levelBlanks + "\n";
+		}
+
+		return resultString;
+	}
+}
---
+++ Unity Runtime Recorder/Scripts/FbxExporter/FbxObjectsManager.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class FbxObjectsManager {
+
+	List<FbxObjectNode> objNodes;
+
+	public FbxObjectsManager () {
+		objNodes = new List<FbxObjectNode> ();
+	}
+
+	// insert new objs into file
+	public void EditTargetFile (string targetFilePath) {
+
+		string sourceData = File.ReadAllText (targetFilePath);
+		string newData = "";
+
+		// find start of the Objects
+		int startIndex = sourceData.IndexOf ("Objects:  {\n");
+		startIndex += ("Objects:  {\n").Length;
+
+		// copy data into new
+		newData = sourceData.Substring(0, startIndex);
+
+
+
+		StringReader reader = new StringReader (sourceData);
+
+		// skip to start index
+		for (int i = 0; i < startIndex; i++)
+			reader.Read ();
+
+
+		// find the end of the Objects {}
+		int bracketBalancer = 1;
+		int readCounter = 0;
+
+		while (true) {
+			char temp = (char)reader.Read ();
+			++readCounter;
+
+			if (temp == '{')
+				bracketBalancer += 1;
+			else if (temp == '}') {
+				bracketBalancer -= 1;
+				if (bracketBalancer == 0)
+					break;
+			}
+		}
+
+		// write other data in new
+		newData += sourceData.Substring(startIndex, readCounter-1);
+
+		// write custom datas
+		for (int i = 0; i < objNodes.Count; i++)
+			newData += objNodes [i].GetResultString ();
+
+
+		// end the file
+		newData += sourceData.Substring(startIndex + readCounter-1);
+
+		File.WriteAllText (targetFilePath, newData);
+	}
+
+	public void AddObject (string nodeType, string nodeId, string nodeName, string subType) {
+		FbxObjectNode obj = new FbxObjectNode (nodeType, nodeId, nodeName, subType);
+	}
+
+	public void AddAnimationCurveNode (string inputId, FbxAnimationCurveNodeType animCurveType, Vector3 initData ) {
+		string nodeName = "";
+
+		if (animCurveType == FbxAnimationCurveNodeType.Translation)
+			nodeName = "AnimCurveNode::T";
+		else if( animCurveType == FbxAnimationCurveNodeType.Rotation)
+			nodeName = "AnimCurveNode::R";
+		else if( animCurveType == FbxAnimationCurveNodeType.Scale)
+			nodeName = "AnimCurveNode::S";
+		else if( animCurveType == FbxAnimationCurveNodeType.Visibility)
+			nodeName = "AnimCurveNode::Visibility";
+		
+		FbxObjectNode obj = new FbxObjectNode ("AnimationCurveNode", inputId, nodeName, "");
+
+		string pName = "Properties70";
+		string pData = " {\n";
+		pData += "\t\t\tP: \"d|X\", \"Number\", \"\", \"A\"," + initData.x.ToString () + "\n";
+		pData += "\t\t\tP: \"d|Y\", \"Number\", \"\", \"A\"," + initData.y.ToString () + "\n";
+		pData += "\t\t\tP: \"d|Z\", \"Number\", \"\", \"A\"," + initData.z.ToString () + "\n";
+		pData += "\t\t}\n";
+
+		obj.AddSubnode (pName, pData);
+
+		objNodes.Add (obj);
+	}
+
+	public void AddAnimationCurve (string inputId, float[] curveData) {
+		FbxObjectNode obj = new FbxObjectNode ("AnimationCurve", inputId, "AnimCurve::", "");
+
+		// prepare some data
+		string[] timeArray = new string[curveData.Length];
+		int[] keyAttrFlagDatas = new int[curveData.Length];
+
+		for (int i = 0; i < timeArray.Length; i++) {
+			timeArray [i] = FbxHelper.getFbxSeconds(i, 60);
+			keyAttrFlagDatas [i] = 24840;
+		}
+
+		// add properties
+		obj.AddSubnode ("Default", "0");
+		obj.AddSubnode ("KeyVer", "4008");
+		obj.AddSubnode ("KeyTime", timeArray);
+		obj.AddSubnode ("KeyValueFloat", curveData);
+		obj.AddSubnode (";KeyAttrFlags", "Cubic|TangeantAuto|GenericTimeIndependent|GenericClampProgressive");
+		obj.AddSubnode ("KeyAttrFlags", new int[]{24840});
+		//obj.AddSubnode (";KeyAttrDataFloat", "RightAuto:0, NextLeftAuto:61.3648; RightAuto:0, NextLeftAuto:0; RightAuto:0, NextLeftAuto:0");
+		obj.AddSubnode ("KeyAttrRefCount", new int[]{timeArray.Length});
+
+		objNodes.Add (obj);
+	}
+}
+
+public enum FbxAnimationCurveNodeType {
+	Translation,
+	Rotation,
+	Scale,
+	Visibility
+}
